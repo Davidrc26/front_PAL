a56f43cd586bb894c8be2c512c9d2c24f6d30fca
Davidrc26 <david.ramirez22171@ucaldas.edu.co>
2025-03-27 07:23:21 -0500

    Refactor user roles representation to use Role interface; update user list display to show roles as a list

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,11 +1,16 @@
 export interface User{
     id: number;
     username: string;
-    roles: Array<string>;
+    roles: Array<Role>;
 }
 
 export interface UserCreate{
     username: string;
     password: string;
     roles: Array<string>;
+}
+
+export interface Role{
+    id: number;
+    name: string;
 }
